5b969a51899254bd793de62d49ad4a39782509a5
SoYoungLEE-me <lsy000626@gmail.com>
2026-02-14 03:17:11 +0900

    health check 추가

--- app.js
+++ app.js
@@ -32,3 +32,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.status(200).send("ok");
+});
