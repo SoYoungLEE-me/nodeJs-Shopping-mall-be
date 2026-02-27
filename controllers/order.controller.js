50e3a889b84b7a713b56c17bf728dc687a718403
SoYoungLEE-me <lsy000626@gmail.com>
2026-02-28 01:51:35 +0900

    order 로직 추가(어드민 관리)

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -42,4 +42,70 @@ orderController.createOrder = async (req, res) => {
   }
 };
 
+orderController.getMyOrders = async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const orders = await Order.find({ userId })
+      .populate("items.productId")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({ status: "success", data: orders });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      error: err.message,
+    });
+  }
+};
+
+orderController.getOrderList = async (req, res) => {
+  try {
+    const { page = 1, ordernum } = req.query;
+    const pageSize = 5;
+
+    const condition = {};
+
+    if (ordernum) {
+      condition.orderNum = { $regex: ordernum, $options: "i" };
+    }
+
+    const totalItemNum = await Order.countDocuments(condition);
+
+    const orderList = await Order.find(condition)
+      .populate("userId")
+      .populate("items.productId")
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
+
+    res.status(200).json({
+      status: "success",
+      data: orderList,
+      totalPageNum: Math.ceil(totalItemNum / pageSize),
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      error: err.message,
+    });
+  }
+};
+
+orderController.updateOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const order = await Order.findByIdAndUpdate(id, { status }, { new: true });
+
+    res.status(200).json({ status: "success", data: order });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      error: err.message,
+    });
+  }
+};
+
 module.exports = orderController;
--- routes/order.api.js
+++ routes/order.api.js
@@ -5,4 +5,20 @@ const router = express.Router();
 
 router.post("/", authController.authenticate, orderController.createOrder);
 
+router.get("/me", authController.authenticate, orderController.getMyOrders);
+
+//어드민 관리
+router.get(
+  "/",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  orderController.getOrderList
+);
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  orderController.updateOrder
+);
+
 module.exports = router;
